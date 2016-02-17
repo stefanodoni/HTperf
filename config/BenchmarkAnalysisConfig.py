922b4ac9eca7ed0eee144e362ee88a61544406c8
Francesco <giglioli.f@libero.it>
2016-02-17 21:51:00 +0100

    Samples

--- config/BenchmarkAnalysisConfig.py
+++ config/BenchmarkAnalysisConfig.py
@@ -6,8 +6,8 @@ __author__ = 'francesco'
 
 # Test parameters (used for Linear Regression and others)
 NUM_SAMPLES = 4 # Number of the first runs to use in the Linear Regression (e.g. first 4 over 10)
-NUM_RUNS = 10 # Number of runs in one test, usually 10 (i.e. number of steps in one growing load ladder)
-NUM_TESTS = 10 # Number of test repetitions, usually 10 (i.e. number of growing load ladders). Globally we have a number of single runs equal to NUM_RUNS * NUM_TESTS
+NUM_RUNS = 1 # Number of runs in one test, usually 10 (i.e. number of steps in one growing load ladder)
+NUM_TESTS = 1 # Number of test repetitions, usually 10 (i.e. number of growing load ladders). Globally we have a number of single runs equal to NUM_RUNS * NUM_TESTS
 
 # TITLE = "RUBBoS Benchmark variable frequencies test\n" \
 #         "Fixed maximum frequencies: 800MHz, 1200MHz, 1600MHz, 2000MHz and 2900MHz\n" \
@@ -24,7 +24,7 @@ TITLE = "CBench Benchmark\n" \
 
 # Runs to be considered in computing real IPC
 START_RUN = 1
-END_RUN = 10
+END_RUN = 1
 
 # Value added to x_max to plot streched line
 X_MAX_PADDING = 50 # CBench
